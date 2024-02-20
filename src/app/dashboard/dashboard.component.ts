f36ec1b77fa61995a888e7ca2898d74a0e9c7501
sevketugurel <ugurelsevket@gmail.com>
2024-02-20 21:27:50 +0300

    area chart eklendi.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,143 +1,539 @@
-import { Component,ViewChild } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { Component, ViewChild } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { NgApexchartsModule } from "ng-apexcharts"
 import {
-  ApexAxisChartSeries,
-  ApexChart,
   ChartComponent,
-  ApexDataLabels,
-  ApexPlotOptions,
-  ApexYAxis,
+  ApexChart,
+  ApexAxisChartSeries,
   ApexTitleSubtitle,
-  ApexXAxis,
+  ApexDataLabels,
   ApexFill,
-  NgApexchartsModule
+  ApexYAxis,
+  ApexXAxis,
+  ApexTooltip,
+  ApexMarkers,
+  ApexAnnotations,
+  ApexStroke
 } from "ng-apexcharts";
-import { BrowserModule } from '@angular/platform-browser';
+import * as ApexCharts from 'apexcharts';
+import { Subscription, interval } from 'rxjs';
+import { data } from './series-data';
+
+
 
 @Component({
   selector: 'app-dashboard',
-  standalone:true,
-  imports: [ NgApexchartsModule,RouterOutlet,CommonModule,CanvasJSAngularChartsModule,MatInputModule,MatButtonModule],
+  standalone: true,
+  imports: [NgApexchartsModule, MatInputModule, MatButtonModule, RouterOutlet, CommonModule, CanvasJSAngularChartsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
-	
-  chartOptions = {
-    title: {
-      text: 'Monthly Sales Data',
-    },
-    theme: 'light2',
-    animationEnabled: true,
-    exportEnabled: true,
-    axisY: {
-      includeZero: true,
-      valueFormatString: '$#,##0k',
-    },
-    data: [
-      {
-        type: 'column', //change type to bar, line, area, pie, etc
-        yValueFormatString: '$#,##0k',
-        color: '#01b8aa',
-        dataPoints: [
-          { label: 'Jan', y: 172 },
-          { label: 'Feb', y: 189 },
-          { label: 'Mar', y: 201 },
-          { label: 'Apr', y: 240 },
-          { label: 'May', y: 166 },
-          { label: 'Jun', y: 196 },
-          { label: 'Jul', y: 218 },
-          { label: 'Aug', y: 167 },
-          { label: 'Sep', y: 175 },
-          { label: 'Oct', y: 152 },
-          { label: 'Nov', y: 156 },
-          { label: 'Dec', y: 164 },
-        ],
-      },
-    ],
-  };
-  addSymbols = (e: any) => {
-	  var suffixes = ["", "K", "M", "B"];
- 
-	  var order = Math.max(Math.floor(Math.log(Math.abs(e.value)) / Math.log(1000)), 0);
-	  if(order > suffixes.length - 1)
-		order = suffixes.length - 1;
- 
-	  var suffix = suffixes[order];
-	  return (e.value / Math.pow(1000, order) + suffix);
-	}
 
-  //multi char 
-	
-	multichartOptions = {
-	  animationEnabled: true,
-	  title:{
-		text: "Oil Reserves and Production "    
-	  },
-	  axisY2: {
-		title:"Production (bbl/day)",
-		includeZero: true,
-		labelFormatter: this.addSymbols
-	  },
-	  axisY: {
-		title: "Reserves(MMbbl)",
-		includeZero: true,
-		labelFormatter: this.addSymbols
-	  },
-	  legend: {
-		cursor: "pointer",
-		itemclick: (e: any) => {
-		  if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-			e.dataSeries.visible = false;
-		  } else {
-			e.dataSeries.visible = true;
-		  }
-		  e.chart.render();
-		}
-	  },
-	  data: [{        
-		  type: "bar",  
-		  showInLegend: true, 
-		  legendText: "Reserves in MMbbl",
-		  dataPoints: [      
-			{ y: 267017, label: "Saudi Arabia" },
-			{ y: 116000, label: "Russia" },
-			{ y: 20682, label: "US" },
-			{ y: 154580, label: "Iran" },
-			{ y: 20350, label: "China" },
-			{ y: 175200, label: "Canda" },
-			{ y: 97800, label: "UAE" },
-			{ y: 297571, label: "Venezuela" }
-		  ]
-		}, {        
-		  type: "bar",  
-		  axisYType: "secondary",
-		  showInLegend: true,
-		  legendText: "production in bbl/day",
-		  dataPoints: [      
-			{ y:11150000, label: "Saudi Arabia" },
-			{ y:10210000, label: "Russia" },
-			{ y:9023000 , label: "US" },
-			{ y:4231000 , label: "Iran" },
-			{ y:4073000 , label: "China" },
-			{ y:3592000, label: "Canda" },
-			{ y:3087000, label: "UAE" },
-			{ y:2453000, label: "Venezuela" }
-		  ]
-		}]
-	}	
+  @ViewChild("chart") chart: ChartComponent | any;
+  public chartOptions: Partial<ChartOptions>;
+
+  @ViewChild("chart2") chart2: ChartComponent | any;
+  public chartOptions2: Partial<ChartOptions>;
+
+  @ViewChild("chart3") chart3: ChartComponent | any;
+  public ChartOptions3: Partial<ChartOptions3>;
+
+  @ViewChild("chart4", { static: false }) chart4: ChartComponent | undefined;
+  public ChartOptions4: Partial<ChartOptions4> | undefined;
+  public activeOptionButton = "all";
+  public updateOptionsData = {
+    "1m": {
+      xaxis: {
+        min: new Date("28 Jan 2013").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "6m": {
+      xaxis: {
+        min: new Date("27 Sep 2012").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "1y": {
+      xaxis: {
+        min: new Date("27 Feb 2012").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    "1yd": {
+      xaxis: {
+        min: new Date("01 Jan 2013").getTime(),
+        max: new Date("27 Feb 2013").getTime()
+      }
+    },
+    all: {
+      xaxis: {
+        min: undefined,
+        max: undefined
+      }
+    }
+  };
+
+
+  constructor() {
+    this.chartOptions = {
+      series: [
+        {
+          data: [
+            {
+              x: "2008",
+              y: [2800, 4500]
+            },
+            {
+              x: "2009",
+              y: [3200, 4100]
+            },
+            {
+              x: "2010",
+              y: [2950, 7800]
+            },
+            {
+              x: "2011",
+              y: [3000, 4600]
+            },
+            {
+              x: "2012",
+              y: [3500, 4100]
+            },
+            {
+              x: "2013",
+              y: [4500, 6500]
+            },
+            {
+              x: "2014",
+              y: [4100, 5600]
+            }
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "rangeBar",
+        zoom: {
+          enabled: false
+        }
+      },
+      plotOptions: {
+        bar: {
+          isDumbbell: true,
+          columnWidth: 4,
+          dumbbellColors: [["#008FFB", "#00E396"]]
+        }
+      },
+      legend: {
+        show: true,
+        showForSingleSeries: true,
+        position: "top",
+        horizontalAlign: "left",
+        customLegendItems: ["Product A", "Product B"]
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          type: "vertical",
+          gradientToColors: ["#00E396"],
+          inverseColors: true,
+          // stops: [0, 100]
+        }
+      },
+      grid: {
+        xaxis: {
+          lines: {
+            show: true
+          }
+        },
+        yaxis: {
+          lines: {
+            show: false
+          }
+        }
+      },
+      xaxis: {
+        tickPlacement: "on"
+      }
+    };
+    this.chartOptions2 = {
+      series: [
+        {
+          name: "Actual",
+          data: [
+            {
+              x: "2011",
+              y: 1292,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 1400,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2012",
+              y: 4432,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 5400,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2013",
+              y: 5423,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 5200,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2014",
+              y: 6653,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 6500,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2015",
+              y: 8133,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 6600,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2016",
+              y: 7132,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 7500,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2017",
+              y: 7332,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 8700,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            },
+            {
+              x: "2018",
+              y: 6553,
+              goals: [
+                {
+                  name: "Expected",
+                  value: 7300,
+                  strokeWidth: 5,
+                  strokeColor: "#775DD0"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      chart: {
+        height: 350,
+        type: "bar"
+      },
+      plotOptions: {
+        bar: {
+          horizontal: true
+        }
+      },
+      colors: ["#00E396"],
+      dataLabels: {
+        formatter: function (val: any, opts: any) {
+          const goals =
+            opts.w.config.series[opts.seriesIndex].data[opts.dataPointIndex]
+              .goals;
+
+          if (goals && goals.length) {
+            return `${val} / ${goals[0].value}`;
+          }
+          return val;
+        }
+      },
+      legend: {
+        show: true,
+        showForSingleSeries: true,
+        customLegendItems: ["Actual", "Expected"],
+        markers: {
+          fillColors: ["#00E396", "#775DD0"]
+        }
+      }
+    };
+    this.ChartOptions3 = {
+      series: [
+        {
+          data: [
+            {
+              x: "INTC",
+              y: 1.2
+            },
+            {
+              x: "GS",
+              y: 0.4
+            },
+            {
+              x: "CVX",
+              y: -1.4
+            },
+            {
+              x: "GE",
+              y: 2.7
+            },
+            {
+              x: "CAT",
+              y: -0.3
+            },
+            {
+              x: "RTX",
+              y: 5.1
+            },
+            {
+              x: "CSCO",
+              y: -2.3
+            },
+            {
+              x: "JNJ",
+              y: 2.1
+            },
+            {
+              x: "PG",
+              y: 0.3
+            },
+            {
+              x: "TRV",
+              y: 0.12
+            },
+            {
+              x: "MMM",
+              y: -2.31
+            },
+            {
+              x: "NKE",
+              y: 3.98
+            },
+            {
+              x: "IYT",
+              y: 1.67
+            }
+          ]
+        }
+      ],
+      legend: {
+        show: false
+      },
+      chart: {
+        height: 350,
+        type: "treemap"
+      },
+      title: {
+        text: "Treemap with color scale"
+      },
+      dataLabels: {
+        enabled: true,
+
+        offsetY: -3
+      },
+      plotOptions: {
+        treemap: {
+          enableShades: true,
+          shadeIntensity: 0.5,
+          reverseNegativeShade: true,
+          colorScale: {
+            ranges: [
+              {
+                from: -6,
+                to: 0,
+                color: "#CD363A"
+              },
+              {
+                from: 0.001,
+                to: 6,
+                color: "#52B12C"
+              }
+            ]
+          }
+        }
+      }
+    };
+    this.initChart();
+  }
+  public generateData(count: any, yrange: any) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+      var x = "w" + (i + 1).toString();
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push({
+        x: x,
+        y: y
+      });
+      i++;
+    }
+    return series;
+  }
+
+  initChart(): void {
+    this.ChartOptions4 = {
+      series: [
+        {
+          data: data
+        }
+      ],
+      chart: {
+        type: "area",
+        height: 350
+      },
+      annotations: {
+        yaxis: [
+          {
+            y: 30,
+            borderColor: "#999",
+            label: {
+              text: "Support",
+              style: {
+                color: "#fff",
+                background: "#00E396"
+              }
+            }
+          }
+        ],
+        xaxis: [
+          {
+            x: new Date("14 Nov 2012").getTime(),
+            borderColor: "#999",
+            label: {
+              text: "Rally",
+              style: {
+                color: "#fff",
+                background: "#775DD0"
+              }
+            }
+          }
+        ]
+      },
+      dataLabels: {
+        enabled: false
+      },
+      markers: {
+        size: 0
+      },
+      xaxis: {
+        type: "datetime",
+        min: new Date("01 Mar 2012").getTime(),
+        tickAmount: 6
+      },
+      tooltip: {
+        x: {
+          format: "dd MMM yyyy"
+        }
+      },
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          // stops: [0, 100]
+        }
+      }
+    };
+  }
+  public updateOptions(option: UpdateOption): void {
+    this.activeOptionButton = option;
+    this.chart.updateOptions(this.updateOptionsData[option], false, true, true);
+  }
+
 }
+type UpdateOption = "1m" | "6m" | "1y" | "1yd" | "all";
+
+interface Xaxis {
+  min: number;
+  max: number;
+}
+
+
+export type ChartOptions4 = {
+  series: ApexAxisChartSeries|any;
+  chart: ApexChart|any
+  dataLabels: ApexDataLabels|any;
+  markers: ApexMarkers|any;
+  title: ApexTitleSubtitle|any;
+  fill: ApexFill|any;
+  yaxis: ApexYAxis|any;
+  xaxis: ApexXAxis|any;
+  tooltip: ApexTooltip|any;
+  stroke: ApexStroke|any;
+  annotations: ApexAnnotations|any;
+  colors: any;
+  grid:ApexGrid;
+  toolbar: any;
+};
+export type ChartOptions3 = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  title: ApexTitleSubtitle;
+  plotOptions: ApexPlotOptions;
+  legend: ApexLegend | any;
+};
+export type ChartOptions2 = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  dataLabels: ApexDataLabels;
+  plotOptions: ApexPlotOptions;
+  legend: ApexLegend;
+  colors: string[];
+};
 export type ChartOptions = {
   series: ApexAxisChartSeries;
   chart: ApexChart;
   dataLabels: ApexDataLabels;
   plotOptions: ApexPlotOptions;
-  yaxis: ApexYAxis;
   xaxis: ApexXAxis;
+  yaxis: ApexYAxis;
+  colors: string[] | any;
   fill: ApexFill;
+  legend: ApexLegend | any;
   title: ApexTitleSubtitle;
+  grid: ApexGrid;
 };
